d589207816dc3f74e25d332056a7b54bcf8ae95c
gargo_elcomp <gargo@estee.com>
2023-11-30 23:50:54 -0800

    refactor

--- lib/2023/1.ex
+++ lib/2023/1.ex
@@ -6,19 +6,13 @@ aoc 2023, 1 do
   """
 
   def p1(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn s -> s |> String.split("", trim: true) |> Enum.filter(&("0" <= &1 and &1 <= "9")) |> calibration() end)
-    |> Enum.sum()
+    compute = fn s -> s |> String.split("", trim: true) |> Enum.filter(&("0" <= &1 and &1 <= "9")) |> then(&String.to_integer("#{hd(&1)}#{List.last(&1)}")) end
+    input |> String.split("\n", trim: true) |> Enum.map(compute) |> Enum.sum()
   end
-
-  def calibration(xs), do: String.to_integer("#{hd(xs)}#{List.last(xs)}")
 
   def p2(input) do
     [{"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}]
     |> Enum.reduce(input, fn {word, number}, s -> String.replace(s, word, "#{word}#{number}#{word}", global: true) end)
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn s -> s |> String.split("", trim: true) |> Enum.filter(&("0" <= &1 and &1 <= "9")) |> calibration() end)
-    |> Enum.sum()
+    |> p1()
   end
 end
