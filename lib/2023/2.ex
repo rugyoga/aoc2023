3e27fbc090d3d839dd29c14461444d0e9dfc0d79
Guy Argo <guy.argo@gmail.com>
2023-12-02 16:55:26 -0800

    day 2 improvements using lists

--- lib/2023/2.ex
+++ lib/2023/2.ex
@@ -1,40 +1,39 @@
 import AOC
+import String, only: [split: 2, to_integer: 1]
+import Enum, only: [map: 2, max: 1, product: 1, reduce: 3, sum: 1, zip_with: 2]
 
 aoc 2023, 2 do
-  def p1(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&parse_game/1 )
-    |> Enum.filter(fn {_, games} -> Enum.all?(games, fn game -> g(game, "red") <= 12 and g(game, "green") <= 13 and g(game, "blue") <= 14 end) end)
-    |> Enum.map(fn {id, _} -> id end)
-    |> Enum.sum()
-  end
-
   def parse_game(line) do
-    [id, games] = String.split(line, ": ", trim: true)
-    games
-    |> String.split("; ", trim: true)
-    |> Enum.map(fn game -> game |> String.split(", ", trim: true) |> Enum.map(fn cubes -> [num, colour] = String.split(cubes, " ", trim: true); {colour, String.to_integer(num)} end) |> Map.new() end)
-    |> then(&{id |> String.replace("Game ", "") |> String.to_integer(), &1})
+    ["Game " <> id, hands] = split(line, ": ")
+    {to_integer(id), hands |> split("; ") |> map(&parse_hand/1)}
   end
 
-  @doc """
-      iex> p2(example_input())
-  """
+  def parse_hand(hand) do
+    hand |> split(", ") |> reduce([0,0,0], &parse_count/2)
+  end
+
+  def parse_count(count, [red, green, blue]) do
+    case split(count, " ") do
+      [n, "red"]   -> [to_integer(n), green, blue]
+      [n, "green"] -> [red, to_integer(n), blue]
+      [n, "blue"]  -> [red, green, to_integer(n)]
+    end
+  end
+
+  def common(input, f) do
+    input |> split("\n") |> map(&(&1 |> parse_game() |> f.())) |> sum()
+  end
+
+  def p1(input) do
+    common(
+      input,
+      fn {id, games} ->
+        [r, g, b] = zip_with(games, &max/1)
+        if(r <= 12 and g <= 13 and b <= 14, do: id, else: 0)
+      end)
+  end
+
   def p2(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&parse_game/1)
-    |> Enum.map(&fewest/1)
-    |> Enum.sum()
+    common(input, fn {_, hands} -> hands |> zip_with(&max/1) |> product() end)
   end
-
-  def fewest({_, games}) do
-    games
-    |> Enum.reduce({0, 0, 0}, fn game, {r, g, b} -> {Enum.max([g(game,"red"), r]), Enum.max([g(game,"green"), g]), Enum.max([g(game, "blue"), b])} end)
-    |> Tuple.to_list()
-    |> Enum.product()
-  end
-
-  def g(m, k), do: Map.get(m, k, 0)
 end
