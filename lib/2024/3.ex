a800f8c85f7221b871e1aa875add82dbed87af6b
Guy Argo <guy.argo@gmail.com>
2024-12-03 10:16:49 -0800

    tidy up the regex

--- lib/2024/3.ex
+++ lib/2024/3.ex
@@ -5,7 +5,7 @@ aoc 2024, 3 do
   def p2(input), do: parse(input, &process_p2/2)
 
   def parse(input, process) do
-    ~r/mul\(([\d]+),([\d]+)\)|do\(\)|don't\(\)/
+    ~r/mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/
     |> Regex.scan(input)
     |> Enum.reduce(0, process)
     |> abs()
