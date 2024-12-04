337393b62c4fed266c4bb90f45166a25b0b784f2
Guy Argo <guy.argo@gmail.com>
2024-12-04 00:42:34 -0800

    tidy up

--- lib/2024/4.ex
+++ lib/2024/4.ex
@@ -3,30 +3,29 @@ import AOC
 aoc 2024, 4 do
   def p1(input) do
     {map, grid} = parse(input)
-    [_ | remaining] = Enum.with_index(["X", "M", "A", "S"])
+    [_ | letters] = Enum.with_index(["X", "M", "A", "S"])
     map["X"]
-    |> Enum.map(&count_xmas(grid, remaining, &1))
+    |> Enum.map(&count_xmas(grid, letters, &1))
     |> Enum.sum()
   end
 
-  def dirs do
-    [{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}]
+  def p2(input) do
+    {map, grid} = parse(input)
+    map["A"]
+    |> Enum.map(&count_x_mas(grid, &1))
+    |> Enum.sum()
   end
 
   def count_xmas(grid, letters, {x_start, y_start}) do
-    dirs()
-    |> Enum.count(fn {x_dir, y_dir} -> Enum.all?(letters, fn {letter, i} -> grid[{x_start + i*x_dir, y_start + i * y_dir}] == letter end) end)
+    [{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}]
+    |> Enum.count(fn {x, y} -> Enum.all?(letters, fn {letter, i} -> grid[{x_start + i*x, y_start + i * y}] == letter end) end)
   end
 
-  def masses do
+  def count_x_mas(grid, {x_start, y_start}) do
     [[{"M", {1, 1}}, {"S", {-1, -1}}, {"M", {1, -1}}, {"S", {-1, 1}}],
      [{"M", {1, 1}}, {"S", {-1, -1}}, {"S", {1, -1}}, {"M", {-1, 1}}],
      [{"S", {1, 1}}, {"M", {-1, -1}}, {"S", {1, -1}}, {"M", {-1, 1}}],
      [{"S", {1, 1}}, {"M", {-1, -1}}, {"M", {1, -1}}, {"S", {-1, 1}}]]
-  end
-
-  def count_x_mas(grid, {x_start, y_start}) do
-    masses()
     |> Enum.count(fn positions -> Enum.all?(positions, fn {letter, {x, y}} -> grid[{x_start+x, y_start+y}] == letter end) end)
   end
 
@@ -41,13 +40,5 @@ aoc 2024, 4 do
         |> Enum.map(fn {ch, x} -> {{x, y}, ch} end)
       end)
     {Enum.group_by(letters, fn {_p, ch} -> ch end, fn {p, _ch} -> p end), Map.new(letters)}
-  end
-
-  def p2(input) do
-    {map, grid} = parse(input)
-
-    map["A"]
-    |> Enum.map(&count_x_mas(grid, &1))
-    |> Enum.sum()
   end
 end
