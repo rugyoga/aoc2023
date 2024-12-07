3d39e49d897b771c4f6f86fa3291fa73594e454c
Guy Argo <guy.argo@gmail.com>
2024-12-06 22:18:35 -0800

    extradry

--- lib/2024/7.ex
+++ lib/2024/7.ex
@@ -1,22 +1,19 @@
 import AOC
 
 aoc 2024, 7 do
-  def p1(input), do: common(input, &makeable?/2)
+  @ops [&Kernel.+/2, &Kernel.*/2]
+  def p1(input), do: common(input, @ops)
+  def p2(input), do: common(input, [&String.to_integer("#{&1}#{&2}")|@ops])
 
-  defp common(input, f) do
+  defp common(input, ops) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn line -> line |> String.replace(":", "") |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1) end)
-    |> Enum.filter(fn [total| nums] -> f.(total, nums) end)
+    |> Enum.filter(fn [total| nums] -> makeable?(total, nums, ops) end)
     |> Enum.map(&hd/1)
     |> Enum.sum()
   end
 
-  def makeable?(total, [accum]), do: total == accum
-  def makeable?(total, [num1, num2 | nums]), do: makeable?(total, [num1+num2| nums]) or makeable?(total, [num1*num2|nums])
-
-  def makeable2?(total, [accum]), do: total == accum
-  def makeable2?(total, [a, b | nums]), do: makeable2?(total, [a+b| nums]) or makeable2?(total, [a*b|nums]) or makeable2?(total, [String.to_integer("#{a}#{b}")|nums])
-
-  def p2(input), do: common(input, &makeable2?/2)
+  def makeable?(total, [accum], _), do: total == accum
+  def makeable?(total, [a, b | nums], ops), do: Enum.any?(ops, fn f -> makeable?(total, [f.(a, b)|nums], ops) end)
 end
