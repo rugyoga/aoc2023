12e35850b23ea88d01363e7d555c94ec0c7fe968
Guy Argo <guy.argo@gmail.com>
2024-12-07 22:32:40 -0800

    dry

--- lib/2024/8.ex
+++ lib/2024/8.ex
@@ -1,48 +1,27 @@
 import AOC
 
 aoc 2024, 8 do
-  def p1(input) do
-    common(input, &antinodes/2)
-  end
+  def p1(input), do: common(input, &pair/4)
+  def p2(input), do: common(input, &multi/4)
 
   def common(input, f) do
     grid = Grid.parse(input, ["."])
     grid.map
     |> Enum.group_by(fn {_p, v} -> v end, fn {p, _v} -> p end)
-    |> Enum.map(fn p -> f.(grid, p) end)
+    |> Enum.map(fn {_value, locations} ->
+      for a <- locations, b <- locations, a < b do
+        f.(grid, a, b, minus(b, a))
+      end
+    end)
     |> List.flatten()
     |> Enum.filter(&Grid.in?(grid, &1))
     |> Enum.uniq()
     |> length()
   end
 
-  def antinodes(grid, {_value, locations}) do
-    for a <- locations, b <- locations, a < b do
-      pair(grid, a, b)
-    end
-  end
-
-  def antinodes2(grid, {_value, locations}) do
-    for a <- locations, b <- locations, a < b do
-      multi(grid, a, b)
-    end
-  end
-
-  def pair(_grid, {a_x, a_y}=a, {b_x, b_y}=b) do
-    {v_x, v_y} = {b_x-a_x, b_y-a_y}
-    [{a_x + 2*v_x, a_y + 2*v_y}, {a_x - v_x, a_y - v_y}]
-  end
-
-  def multi(grid, {a_x, a_y}=a, {b_x, b_y}=b) do
-    {v_x, v_y} = {b_x-a_x, b_y-a_y}
-    [line(grid, a, {-v_x, -v_y}), line(grid, b, {v_x, v_y})]
-  end
-
-  def line(grid, {x, y} = p, {d_x, d_y} = v) do
-    if(Grid.in?(grid, p), do: [p | line(grid, {x + d_x, y + d_y}, v)], else: [] )
-  end
-
-  def p2(input) do
-    common(input, &antinodes2/2)
-  end
+  def plus({a_x, a_y}, {b_x, b_y}), do: {a_x + b_x, a_y + b_y}
+  def minus({a_x, a_y}, {b_x, b_y}), do: {a_x - b_x, a_y - b_y}
+  def pair(_grid, a, b, v), do: [minus(a, v), plus(b, v)]
+  def multi(grid, a, b, v), do: [line(grid, a, minus({0, 0}, v)), line(grid, b, v)]
+  def line(grid, p, v), do: if(Grid.in?(grid, p), do: [p | line(grid, plus(p, v), v)], else: [])
 end
