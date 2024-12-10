3cc3a962b8dea7bf82ad27d25959ba1d268749bb
Guy Argo <guy.argo@gmail.com>
2024-12-10 09:55:22 -0800

    drier

--- lib/2024/10.ex
+++ lib/2024/10.ex
@@ -1,45 +1,36 @@
 import AOC
 
 aoc 2024, 10 do
-  def p1(input) do
-    {grid, by_num} = common(input)
-    by_num[0]
-    |> Enum.map(&counter(&1, grid, by_num))
-    |> Enum.sum
-  end
+  def p1(input), do: common(input, &count1/2)
+  def p2(input), do: common(input, &count2/2)
 
-  def p2(input) do
-    {grid, by_num} = common(input)
-    by_num[0]
-    |> Enum.map(&count2(&1, grid, by_num))
-    |> Enum.sum()
-  end
-
-  def count2({_, 9}, _, _), do: 1
-  def count2({p, i}, grid, by_num) do
+  def count2({_, 9}, _), do: 1
+  def count2({p, i}, grid) do
     grid
     |> Grid.neighbours_4(p)
     |> Enum.filter(fn n -> grid.map[n] == i + 1 end)
-    |> Enum.map(&count2({&1, i + 1}, grid, by_num))
+    |> Enum.map(&count2({&1, i + 1}, grid))
     |> Enum.sum()
   end
 
-  def count({p, 9}, _, _), do: [p]
-  def count({p, i}, grid, by_num) do
+  def count1_rec({p, 9}, _), do: [p]
+  def count1_rec({p, i}, grid) do
     grid
     |> Grid.neighbours_4(p)
     |> Enum.filter(fn n -> grid.map[n] == i + 1 end)
-    |> Enum.flat_map(&count({&1, i + 1}, grid, by_num))
+    |> Enum.flat_map(&count1_rec({&1, i + 1}, grid))
   end
 
-  def counter(p, grid, by_num) do
-    count(p, grid, by_num)
+  def count1(p, grid) do
+    count1_rec(p, grid)
     |> Enum.uniq
     |> length()
   end
 
-  def common(input) do
+  def common(input, f) do
     grid = Grid.parse(input, [], &String.to_integer/1)
-    {grid, Enum.group_by(grid.map, fn {_p, i} -> i end)}
+    Enum.group_by(grid.map, fn {_p, i} -> i end)[0]
+    |> Enum.map(&f.(&1, grid))
+    |> Enum.sum()
   end
 end
