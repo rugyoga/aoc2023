0c0bd1137af25ba1c3e0fa479b3ce5ce2576a71c
Guy Argo <guy.argo@gmail.com>
2024-12-11 18:55:32 -0800

    memoize

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule Aoc2023.MixProject do
       {:advent_of_code_utils, "~> 4.0"},
       {:libgraph, "~> 0.16.0"},
       {:math, "~> 0.7.0"},
+      {:memoize, "~> 1.4"},
       {:nx, "~> 0.6.4"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
